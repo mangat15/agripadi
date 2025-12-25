e2465cb7ae5ae1a18d0bbae98115d61e1774c330
unknown <hashimrusli1506@gmail.com>
2025-12-25 22:26:21 +0800

    Enable SQLite foreign key constraints in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,19 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Enable foreign key constraints for SQLite
+        if (DB::connection() instanceof \Illuminate\Database\SQLiteConnection) {
+            DB::statement('PRAGMA foreign_keys=ON');
+        }
+    }
 }
